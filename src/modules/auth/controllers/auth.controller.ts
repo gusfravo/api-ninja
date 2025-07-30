90bf4781c767106013bed6886f1a9f6eced87473
Gustavo Solano <gustavo.esp@icloud.com>
2025-07-29 18:53:19 -0600

    feat: added docs to crate method

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { RegisterUser } from '@auth/dtos/register-user.dto';
+import { User } from '@auth/entities/user.entity';
 import { AuthService } from '@auth/services/auth/auth.service';
 import {
   Body,
@@ -7,11 +8,20 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Metodo para crear un usuario' })
+  @ApiBody({
+    type: RegisterUser,
+  })
+  @ApiResponse({
+    type: User,
+  })
   @Post('create')
   @UsePipes(new ValidationPipe())
   create(@Body() createUserDto: RegisterUser) {
--- src/modules/auth/dtos/register-user.dto.ts
+++ src/modules/auth/dtos/register-user.dto.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsString, IsStrongPassword, IsUUID } from 'class-validator';
 
 export class RegisterUser {
+  @ApiProperty({ description: 'username de la cuenta' })
   @IsString()
   username: string;
 
+  @ApiProperty({ description: 'Contraseña del usuario' })
   @IsStrongPassword()
   @IsString()
   password: string;
 
+  @ApiProperty({ description: 'Nombre completo de la cuenta de usuario' })
   @IsString()
   fullName: string;
 
+  @ApiProperty()
   @IsBoolean()
   status: boolean;
 
+  @ApiProperty({ description: 'Nombre del rol a asignar a la cuenta' })
   @IsString()
   roleName: string;
 }
