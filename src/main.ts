71582f8f7511fa3b166a24c175837aad52df44dd
Gustavo Solano <gustavo.esp@icloud.com>
2025-07-29 18:43:18 -0600

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  initSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+const initSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Ninja Docs API')
+    .setDescription('The Ninja API description')
+    .setVersion('1.0')
+    .addTag('ninja')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+};
