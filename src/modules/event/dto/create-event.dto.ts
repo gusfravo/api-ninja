ba2910d4a5650839458d06e721c89e434c64afdb
Gustavo Solano <gustavo.esp@icloud.com>
2025-08-05 19:03:03 -0600

    feat: added update and create event dtos

---
+++ src/modules/event/dto/create-event.dto.ts
@@ -0,0 +1,24 @@
+import { EventStatus } from '@event/enums/event-status.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class CreateEvent {
+  @ApiProperty({ description: 'Estatus used for that event' })
+  @IsString()
+  status: EventStatus;
+
+  @ApiProperty({ description: 'Fecha de inicio de la prestacion' })
+  @IsDate()
+  startDate: Date;
+
+  @ApiProperty({ description: 'FEcha de fin del evento' })
+  @IsDate()
+  @IsOptional()
+  endDate: Date | undefined;
+
+  @ApiProperty({
+    description: 'identificador de la prestacion a asociiar con el evento',
+  })
+  @IsUUID()
+  benefitId: string;
+}
